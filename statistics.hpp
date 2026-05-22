933d6fe873d224c56f59632610033ffc7b667f91
яна <imaksimmar@gmail.com>
2026-05-22 23:49:52 +0300

    added Get/Set, changed constructors

--- statistics.hpp
+++ statistics.hpp
@@ -2,91 +2,32 @@
 #include "stream.hpp"
 #include "exceptions.hpp"
 
-/* template <typename T>
-class Statistics {
-private:
-    size_t count;    
-    T sum;          
-    T min;          
-    T max;    
-    double average;     
-
-public:
-    Statistics()  
-    : count(0), sum(T()), min(T()), max(T()), average(0.0) {}
-
-    size_t GetCount() const { return count; }
-    T GetSum() const { return sum; }
-    T GetMin() const { return min; }
-    T GetMax() const { return max; }
-    double GetAverage() const { return average; }
-
-    void Reset() 
-    {
-        count = 0;
-        sum = T();
-        min = T();
-        max = T();
-        average = 0.0;
-    }
-
-    void Update(T item) 
-    {
-        if(count==0) {
-            min = item;
-            max = item;
-        }
-        count++;
-        sum += item;
-
-        if(item<min) { min = item; }
-        if(item>max) { max = item; }
-
-        average = static_cast<double>(sum)/count;
-    }
-// по констурктуру статистики 
-    void Process(ReadOnlyStream<T>& stream, size_t n) 
-    {
-        for(size_t i=0; i<n; i++) {
-            if(stream.IsEndOfStream()) { break; }
-            Update(stream.Read());
-        }
-    }
-};
-*/
-
-
 template <typename T>
 class Statistics {
 private:
-    size_t count;    
+    size_t count; 
     T sum;          
     T min;          
     T max;    
-    double average;
+    double average; 
 
-    void Update(T item) 
-    {
-        if(count==0) {
-            min = item;
-            max = item;
-        }
-        count++;
-        sum += item;
-        if(item<min) { min = item; }
-        if(item>max) { max = item; }
-        average = static_cast<double>(sum)/count;
-    }
-
-public:
+public: 
     Statistics()  
     : count(0), sum(T()), min(T()), max(T()), average(0.0) {}
 
+    Statistics(LazySequence<T>& seq)
+    : count(0), sum(T()), min(T()), max(T()), average(0.0)
+    {
+        ReadOnlyStream<T> stream(seq);
+        while(!stream.IsEndOfStream()) {  
+            Update(stream.Read());
+        }
+    }
 
     Statistics(LazySequence<T>& seq, size_t n)
     : count(0), sum(T()), min(T()), max(T()), average(0.0)
     {
-        ReadOnlyStream<T> stream(&seq);
+        ReadOnlyStream<T> stream(seq);
         for(size_t i=0; i<n; i++) {
             if(stream.IsEndOfStream()) { break; }
             Update(stream.Read());
@@ -99,6 +40,12 @@ public:
     T GetMax() const { return max; }
     double GetAverage() const { return average; }
 
+    void SetCount(size_t cnt) { count = cnt; }
+    void SetSum(T s) { sum = s; }
+    void SetMin(T mn) { min = mn; }
+    void SetMax(T mx) { max = mx; }
+    void SetAverage(double avg) { average = avg; }
+
     void Reset() 
     {
         count = 0;
@@ -106,5 +53,19 @@ public:
         min = T();
         max = T();
         average = 0.0;
+    }
+
+    void Update(T item) 
+    {
+        if(count==0) {
+            min = item;
+            max = item;
+        }
+        count++;
+        sum += item;
+        if(item<min) { min = item; }
+        if(item>max) { max = item; }
+        
+        average = static_cast<double>(sum)/count;
     }
 };
