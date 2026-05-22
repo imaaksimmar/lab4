1850c7978c4211e9a079b9e344874f93c73ac4e9
яна <imaksimmar@gmail.com>
2026-05-22 23:49:00 +0300

    no ownsource

--- stream.hpp
+++ stream.hpp
@@ -6,91 +6,71 @@
 template <typename T>
 class ReadOnlyStream {
 private:
-    LazySequence<T>* source;
+    LazySequence<T>& source;
     size_t position;
     bool isFinite;
-    bool ownsSource; 
 
 public:
-    ReadOnlyStream(Sequence<T>* sequence) : position(0), isFinite(true), ownsSource(true)
+    ReadOnlyStream(LazySequence<T>& lazySeq)
+    : source(lazySeq), position(0)
     {
-        if(sequence==nullptr) { throw NullPointer(); }
-        source = new LazySequence<T>(sequence);
+        isFinite = !lazySeq.GetLength().IsInfinite();
     }
 
-    ReadOnlyStream(LazySequence<T>* lazySeq) : position(0), ownsSource(false)
-    {
-        if(lazySeq==nullptr) { throw NullPointer(); }
-        source = lazySeq;
-        isFinite = !lazySeq->GetLength().isInfinite;
-    }
-
-    ~ReadOnlyStream() { if(ownsSource) { delete source; } }
-
-    bool IsEndOfStream() const 
+    bool IsEndOfStream() const
     {
         if(!isFinite) { return false; }
-        return position >= source->GetMaterializedCount();
+        return position >= source.GetMaterializedCount();
     }
 
-    T Read() 
+    T Read()
     {
         if(IsEndOfStream()) { throw EndOfStream(); }
-        T item = source->Get(position);
+        T item = source.Get(position);
         position++;
         return item;
     }
 
+    T& operator[](size_t index)
+    {
+        if(isFinite && index >= source.GetMaterializedCount()) {
+            throw IndexOutOfRange(index, source.GetMaterializedCount());
+        }
+        return source[index];
+    }
+
     size_t GetPosition() const { return position; }
 
-    bool IsCanSeek() const { return true; }
-
-    size_t Seek(size_t index) 
+    size_t Seek(size_t index)
     {
-        if(isFinite && index>=source->GetMaterializedCount()) {
-            throw IndexOutOfRange(index, source->GetMaterializedCount());
+        if(isFinite && index >= source.GetMaterializedCount()) {
+            throw IndexOutOfRange(index, source.GetMaterializedCount());
         }
         position = index;
         return position;
     }
 
-    bool IsCanGoBack() const { return true; }
-
-    void Open() {}
+    void Open() {} 
     void Close() { position = 0; }
 };
+
 
 
 template <typename T>
 class WriteOnlyStream {
 private:
-    LazySequence<T>* target;
+    LazySequence<T>& target;
     size_t position;
-    bool ownsTarget; 
 
 public:
-    WriteOnlyStream(Sequence<T>* sequence) : position(0), ownsTarget(true)
-    {
-        if(sequence==nullptr) { throw NullPointer(); }
-        target = new LazySequence<T>(sequence);
-    }
-
-    WriteOnlyStream(LazySequence<T>* lazySeq) : position(0), ownsTarget(false)
-    {
-        if(lazySeq==nullptr) { throw NullPointer(); }
-        target = lazySeq;
-    }
-
-    ~WriteOnlyStream() 
-    { 
-        if(ownsTarget) { delete target; }
-    }
+    WriteOnlyStream(LazySequence<T>& lazySeq)
+    : target(lazySeq), position(0) {}
 
     size_t GetPosition() const { return position; }
 
-    size_t Write(T item) 
+    size_t Write(T item)
     {
-        target->Append(item);
+        target.Append(item);
         position++;
         return position;
     }
